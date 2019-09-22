5797a4bef5e359b662d15c087e03f368492e7481
Alejo Castaño Rojas <alejocas@experimentality.co>
2019-09-22 15:34:27 -0500

    Router outlet problem solved

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { LayoutComponent } from './layout.component';
 import { MaterialModule } from '../material.module';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { RoutesModule } from '../routes/routes.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
     declarations:[LayoutComponent],
-    imports: [MaterialModule,FlexLayoutModule],
+    imports: [MaterialModule,FlexLayoutModule, RoutesModule],
     exports: [LayoutComponent],
     providers:[MediaMatcher]
 })
